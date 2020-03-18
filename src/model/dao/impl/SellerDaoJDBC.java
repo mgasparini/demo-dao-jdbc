605ae8f7447870f8f29d86b1805e2c32ac9e556f
mgasparini <marcelorgasparini@hotmail.com>
2020-03-18 20:18:30 -0300

    seller dao jdbc

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
